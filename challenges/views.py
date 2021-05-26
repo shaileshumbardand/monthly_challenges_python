f3f5b4b748e670e03599765c58980d032db3548e
shailesh <shaileshumbardand@gmail.com>
2021-05-26 09:08:46 +0530

    dynamic url update

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges = {
+    "january": "This is January",
+    "february": "This is february",
+    "march": "This is march",
+    "april": "This is april",
+    "may": "This is may",
+    "jun": "This is jun",
+    "july": "This is july",
+    "august": "This is august",
+    "september": "This is september",
+    "october": "This is october",
+    "november": "This is november",
+    "december": "This is december",
+}
+
 
 # Create your views here.
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
